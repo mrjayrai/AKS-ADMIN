e11f650a8e12e2cd85c0b775460258854f566806
mrjayrai <raijai127@gmail.com>
2025-05-28 17:40:26 +0530

    Added Logo

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -88,14 +88,14 @@ const AppHeader: React.FC = () => {
               width={154}
               height={32}
               className="dark:hidden"
-              src="./images/logo/logo.svg"
+              src="/images/logo/logo-1-removebg.png"
               alt="Logo"
             />
             <Image
               width={154}
               height={32}
               className="hidden dark:block"
-              src="./images/logo/logo-dark.svg"
+              src="/images/logo/logo-dark1-removebg.png"
               alt="Logo"
             />
           </Link>
