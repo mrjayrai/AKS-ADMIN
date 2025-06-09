a1b4ac2fc8ee87b0eb9e05971d819959e5b057dd
mrjayrai <raijai127@gmail.com>
2025-06-09 16:45:08 +0530

    Corrected Sign In Page and change UI for Sign Up Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import SignIn from "@/app/(full-width-pages)/(auth)/signin/page";
+import { redirect } from "next/navigation";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,5 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function Sign() {
-  return <SignIn />;
+ redirect("/signin");
 }
